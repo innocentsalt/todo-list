11e7d21c935c940fef1f2493226e18c1944111ba
innocentsalt <innocentsalt@tutanota.com>
2021-05-25 18:51:35 +0530

    Add popup for new project

--- src/index.js
+++ src/index.js
@@ -2,8 +2,30 @@ import './style.css'
 
 const hamburger = document.querySelector('.hamburger')
 const sidebar = document.querySelector('.sidebar')
+const addProject = document.querySelector('.add-project')
+const addProjectPopup = document.querySelector('.add-project-popup')
+const addProjectCancel = document.querySelector('.form-container .cancel')
+const projectTitle = document.querySelector('.project-title')
 
 hamburger.addEventListener('click', () => {
+  if (addProjectPopup.style.display !== 'none') {
+    addProjectPopup.style.display = 'none'
+  }
+  sidebar.classList.toggle('active')
+  hamburger.classList.toggle('active')
+})
+
+addProject.addEventListener('click', () => {
+  addProjectPopup.style.display = 'block'
+  if (sidebar.classList.contains('active')) {
+    sidebar.classList.toggle('active')
+    hamburger.classList.toggle('active')
+  }
+})
+
+addProjectCancel.addEventListener('click', () => {
+  projectTitle.value = ''
+  addProjectPopup.style.display = 'none'
   sidebar.classList.toggle('active')
   hamburger.classList.toggle('active')
 })
